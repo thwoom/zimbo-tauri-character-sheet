cbaab91b1a4fe7ebab3ebe8b0efaacf6b08b087f
thwoom <dan.hubel@gmail.com>
2025-08-15 07:33:53 -0700

    chore: add tsconfig

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { CharacterProvider } from './state/CharacterContext.jsx';
-import { ThemeProvider } from './state/ThemeContext.jsx';
 import { SettingsProvider } from './state/SettingsContext.jsx';
+import { ThemeProvider } from './state/ThemeContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

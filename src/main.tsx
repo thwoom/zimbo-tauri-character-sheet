6c5c9f5989555697a5ed1b586702d03c5cc36f24
thwoom <dan.hubel@gmail.com>
2025-08-16 23:38:33 -0700

    docs: describe runtime error handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ErrorBoundary from './components/ErrorBoundary';
 import { CharacterProvider } from './state/CharacterContext';
 import { SettingsProvider } from './state/SettingsContext';
 import { ThemeProvider } from './state/ThemeContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <CharacterProvider>
-        <SettingsProvider>
-          <App />
-        </SettingsProvider>
-      </CharacterProvider>
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ThemeProvider>
+        <CharacterProvider>
+          <SettingsProvider>
+            <App />
+          </SettingsProvider>
+        </CharacterProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
   </React.StrictMode>,
 );
